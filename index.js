bb66a7e1f78fd03e005ceee6b1039c80461badd8
DoomerCreatine <doomercreatine@outlook.com>
2022-08-23 11:01:39 -0400

    updates

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const tmi = require('tmi.js');
+const fetch = require('node-fetch');
+const fs = require('fs');
 // Users that can use certain bot commands
 var whitelist = ['hey_jase', 'doomercreatine']
 const client = new tmi.Client({
@@ -8,43 +10,78 @@ const client = new tmi.Client({
   },
   identity: {
     username: 'botcreatine',
-    password: ''
+    password: process.env.apiKey
   },
   channels: [ 'doomercreatine' ]
 });
 
 client.connect();
-/*
-client.on('chat', (channel, tags, message, self) => {
-  console.log(`${tags['display-name']}: ${message}`);
-});
-*/
-
 
 // Declare the global variables needed for guess logging
 var logging_guess = false;
 var current_guesses = {};
-var current_guesses = {};
 var current_counts = 0;
 
-client.on('chat', (channel, tags, message, self) => {
-  if(self || !message.startsWith('?')) {
-    return;
-  }
-
-  const args = message.slice(1).split(' ');
-  const command = args.shift().toLowerCase();
-  console.log(`${args[0]}`)
-  if(whitelist.includes(tags.username.toLowerCase())) {
-    if (command === 'botcheck') {
-      client.say(channel, `/me is online and running @${tags.username} POGGIES`)
-    } else if(command === 'start') {
-      //client.say(channel, `@${tags.username}, you said: ${args.join(' ')}`);
-      if (tags.username.toLowerCase == 'hey_jase' & !logging_guess) {
-        logging_guess = true;
-        current_guesses = {};
-        current_guesses = {};
-      }
-    }
-  } 
+// Print out information on room joined
+client.on("roomstate", (channel, state) => {
+  console.log(`Joined channel: ${channel}\nBot name: ${client.getUsername()}`);
 });
+
+client.on('chat', (channel, tags, message, self) => {
+  if(message.startsWith('?')) {
+    const args = message.slice(1).split(' ');
+    const command = args.shift().toLowerCase();
+  
+    if(whitelist.includes(tags.username.toLowerCase())) {
+      if (command === 'botcheck') {
+        client.say(channel, `/me is online and running @${tags.username} POGGIES`)
+      } else if(command === 'start' && !logging_guess) {        
+        get_userlist();
+        current_guesses = {};
+        logging_guess = true;
+        console.log(logging_guess);
+        
+      } else if(command === 'end'){
+        // Handle ending the guesses
+        console.log(current_guesses);
+      } else if(command === 'winner') {
+        // Handle choosing a winner
+        let casket_val = parseInt(args[0]);
+        console.log(casket_val);
+      }
+    } 
+  } else {
+    if(logging_guess){
+      console.log("Message received");
+      // log guesses
+      current_guesses[tags.username.toLowerCase()] = message;
+    }
+  }  
+});
+
+
+// Fetch current userlist so we can filter out names
+function get_userlist() {
+  let url = "https://tmi.twitch.tv/group/user/hey_jase/chatters";
+  
+  
+  async function getData() {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+  }
+  
+  const userlist = (async () => { const data = await getData(); return data })()
+  userlist.then(function(result) {
+      var users = [];
+      users = users.concat(result['chatters']['moderators'], result['chatters']['viewers']);
+      fs.writeFile("./current_users.json", JSON.stringify(users), function(err) {
+          if(err) {
+                console.log(err);
+          } 
+          else {
+            console.log("Output saved to current_users.json");
+          }
+        });
+  });
+}
--- lib.js
+++ lib.js
@@ -1,7 +1,7 @@
 // RegEx expression to parse messages for the numeric guess
 var guess_reg = new RegExp('(?<![\/\\aAcCdDeEfFgGhHiIjJlLnNoOpPqQrRsStTuUvVwWxXyYzZ])[0-9\s,.]+(?![\/\\aAcCdDeEfFgGhHiIjJlLnNoOpPqQrRsStTuUvVwWxXyYzZ]+\b)\s*[,.]*[kKmMbB]{0,1}\s*[0-9]*');
 var handle_red = new RegExp('([@#][A-Za-z0-9_]+)|(\w+:\/\/\S+)');
-function extract_guess(guess) {
+export function extract_guess(guess) {
     var match;
     var res = [];
     match = reg.exec(guess);
@@ -12,18 +12,27 @@ function extract_guess(guess) {
     }
 }
 const fetch = require('node-fetch');
-let url = "https://tmi.twitch.tv/group/user/hey_jase/chatters";
-
-
-async function getData() {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+export function get_userlist() {
+    let url = "https://tmi.twitch.tv/group/user/hey_jase/chatters";
+    
+    
+    async function getData() {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    }
+    
+    const userlist = (async () => { const data = await getData(); return data })()
+    userlist.then(function(result) {
+        var users = [];
+        users = users.concat(result['chatters']['moderators'], result['chatters']['viewers']);
+        fs.writeFile("./current_users.json", JSON.stringify(users), function(err) {
+            if(err) {
+                  console.log(err);
+            } 
+            else {
+              console.log("Output saved to current_users.json");
+            }
+          });
+    });
 }
-
-const userlist = (async () => { const data = await getData(); return data })()
-userlist.then(function(result) {
-    var users = [];
-    users = users.concat(result['chatters']['moderators'], result['chatters']['viewers']);
-    console.log(users);
-});
